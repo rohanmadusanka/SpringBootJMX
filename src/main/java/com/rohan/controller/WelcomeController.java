0c71ae95e60170ad12ec82a006665099b4aaf031
Rohan Madusanka <rmadusanka@virtusapolaris.com>
2017-08-08 09:05:04 +0530

    Added_comments

--- src/main/java/com/rohan/controller/WelcomeController.java
+++ src/main/java/com/rohan/controller/WelcomeController.java
@@ -14,18 +14,37 @@ import com.rohan.model.JMXModel;
 import com.rohan.service.IOService;
 import com.rohan.service.JMXService;
 
+/**
+ * @author rmadusanka
+ *
+ */
 @Controller
 public class WelcomeController {
 
 	JMXService jmxService = new JMXService();
 	IOService ioService=new IOService();
 
+	
+	/**
+	 * 
+	 * @param model
+	 * @return String 
+	 * 
+	 * This method will return page name to view welcome.jsp page
+	 */
 	@RequestMapping("/")
 	public String welcome(Map<String, Object> model) {
-		
 		return "welcome";
 	}
 	
+	
+	
+	/**
+	 * 
+	 * @param model
+	 * @return String
+	 * This method will return page name to view jmxValues.jsp page
+	 */
 	@RequestMapping("/getJMXValues")
 	public String getJMXValues(Map<String, Object> model) {
 		
@@ -43,6 +62,12 @@ public class WelcomeController {
 		return "jmxValues";
 	}
 
+	/**
+	 * 
+	 * @return HashMap
+	 * This method will invoke read file method in service 
+	 * layer and return it's hashmap object
+	 */
 	@RequestMapping("/readText")
 	@ResponseBody
 	public Map<Integer,String> readTextFile() {
@@ -50,6 +75,12 @@ public class WelcomeController {
 		return ioService.readFile();
 	}
 
+	/**
+	 * 
+	 * @return HashSet
+	 * This method will invoke getJMXObject method in service 
+	 * layer and return it's Set<String> object 
+	 */
 	@RequestMapping(value="/getDomains")
 	@ResponseBody
 	public Set<String> getDomains() {
@@ -63,7 +94,14 @@ public class WelcomeController {
 		return null;
 	}
 	
-	
+	/**
+	 * 
+	 * @param domainid
+	 * @return Set<String>
+	 * 
+	 * This method will take domain id as a parameter and pass it to getSpecificJMXObject
+	 * method in service layer and return it's Set<String> Object
+	 */
 	@RequestMapping(value="/domain/{domainid}/getTypes", method = RequestMethod.POST)
 	@ResponseBody
 	public Set<String> getTypes(@PathVariable String domainid) {
@@ -75,6 +113,15 @@ public class WelcomeController {
 		return null;
 	}
 
+	/**
+	 * 
+	 * @param domainid
+	 * @param typeid
+	 * @return Set<String>
+	 * 
+	 * This method will take domainid,typeid as a parameter and pass it to getSpecificKeyProperty
+	 * method in service layer and return it's Set<String> Object
+	 */
 	@RequestMapping(value="/domain/{domainid}/type/{typeid}/getNames", method = RequestMethod.POST)
 	@ResponseBody
 	public Set<String> getNames(@PathVariable String domainid,@PathVariable String typeid) {
@@ -87,7 +134,16 @@ public class WelcomeController {
 	}
 	
 	
-	
+	/**
+	 * 
+	 * @param domainid
+	 * @param typeid
+	 * @param nameid
+	 * @return Set<String>
+	 * 
+	 * This method will take domainid,typeid,nameid as a parameter and pass it to getIndividualAttributes
+	 * method in service layer and return it's Set<String> Object 
+	 */
 	@RequestMapping(value="/domain/{domainid}/type/{typeid}/name/{nameid}/getAttributes", method = RequestMethod.POST)
 	@ResponseBody
 	public Set<String> getFinalAttributes(@PathVariable String domainid,@PathVariable String typeid,@PathVariable String nameid) {
@@ -103,7 +159,17 @@ public class WelcomeController {
 	}
 	
 	
-	
+	/**
+	 * 
+	 * @param domainid
+	 * @param typeid
+	 * @param nameid
+	 * @param attributeid
+	 * @return String
+	 * 
+	 * This method will take domainid,typeid,nameid,attributeid as a parameter and pass it to getIndividualAttributes
+	 * method in service layer and return it's String Object 
+	 */
 	@RequestMapping(value="/domain/{domainid}/type/{typeid}/name/{nameid}/attribute/{attributeid}/getValue", method = RequestMethod.POST)
 	@ResponseBody
 	public String getValue(@PathVariable String domainid,@PathVariable String typeid,@PathVariable String nameid,@PathVariable String attributeid) {
@@ -111,8 +177,6 @@ public class WelcomeController {
 			if(nameid.equals("undefined")){
 				nameid=null;
 			}
-			
-			
 			return jmxService.getSpecificKeyProperty(domainid, typeid, nameid,attributeid);
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
@@ -122,19 +186,29 @@ public class WelcomeController {
 	}
 	
 	
-	
+	/**
+	 * 
+	 * @return String
+	 * 
+	 * This method will invoke fillDataFile method in service layer
+	 */
 	@RequestMapping("/fillTable")
 	@ResponseBody
 	public String fillTable() {
-		System.out.println("Called fill Table method");
 		return ioService.fillDataFile();
 		
 	}
 	
+	/**
+	 * 
+	 * @return String 
+	 * 
+	 * This method will invoke openTextFile method in service layer 
+	 */
 	@RequestMapping("/openFile")
 	@ResponseBody
 	public String openFile() {
-		System.out.println("Called fill Table method");
+		
 		return ioService.openTextFile();
 		
 	}
